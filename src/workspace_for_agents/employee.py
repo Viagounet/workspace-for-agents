0c8f25b4acc69263088bae5c3af64422c572b65a
Viagounet <ismael.rousseau@gmail.com>
2024-12-23 14:25:46 +0100

    ruff formating

--- src/workspace_for_agents/employee.py
+++ src/workspace_for_agents/employee.py
@@ -2,7 +2,14 @@ from typing import Self
 
 
 class Employee:
-    def __init__(self, id: int, name: str, email: str, contacts_map: dict[int, Self], additional_information: str):
+    def __init__(
+        self,
+        id: int,
+        name: str,
+        email: str,
+        contacts_map: dict[int, Self],
+        additional_information: str,
+    ):
         self.id = id
         self.name = name
         self.contacts_map = contacts_map
