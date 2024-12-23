71854dd464a7059981dc60a4c331946cbc85a062
Viagounet <ismael.rousseau@gmail.com>
2024-12-23 14:25:00 +0100

    add test env data

--- src/workspace_for_agents/employee.py
+++ src/workspace_for_agents/employee.py
@@ -2,7 +2,8 @@ from typing import Self
 
 
 class Employee:
-    def __init__(self, name: str, contacts: list[Self]):
+    def __init__(self, id: int, name: str, email: str, contacts_map: dict[int, Self], additional_information: str):
+        self.id = id
         self.name = name
-        self.contacts = contacts
+        self.contacts_map = contacts_map
         self.known_facts: list[str] = []
